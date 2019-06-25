c964d1135caafb8e92180e5304ab512e5f350ed4
GABRIEL HENRIQUE DE PAULA PARDINHO <gabriel.hppardinho@senacsp.edu.br>
2019-06-25 17:18:59 -0300

    Meu Portfolio Pronto

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+//document.ready
+$(function(){
+    var mixer = mixitup('#mix-container');
+});
+
+function Enviar (){
+    var nome = document.getElementById("nomeid");
+    var email = document.getElementById("emailid");
+    var telefone = document.getElementById("telid");
+    var mc = document.getElementById("mcid");
+    if(nome.value && email.value && telefone.value && mc.value != ""){
+        alert('Obrigado' + nome.value + 'entrarei em contato em breve');
+    }
+}
